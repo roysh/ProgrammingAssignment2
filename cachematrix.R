516223ffa6c76e28214046285a2de0fd9aa4fea9
Shantanu Roy <shantanuroy@gmail.com>
2015-01-25 20:57:30 +0100

    1

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,7 @@
+# Here, we are exploiting the cache to avoid repetitive computation on the unchanged data.
+# Function "makeCacheMatrix" returns a list of 4 values:
+# 1. Set a matrix, 2. Get a matrix, 3. Set the inverse and 4. Get the inverse 
+
 makeCacheMatrix <- function(x = matrix()) {
   myinv <- NULL
   set <- function(y) {
@@ -10,6 +14,10 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
+# cacheSolve returns the inverse of the matrix either through calculating a-fresh or getting from the cache
+# through the R function "solve"
+
+ 
 cacheSolve <- function(x, ...) {
   myinv <- x$getinv()
   if(!is.null(myinv)) {
@@ -17,9 +25,8 @@ cacheSolve <- function(x, ...) {
       return(myinv)
   }
   mydata <- x$get()
-  myinv <- solve(mydata)
+      message("calculating the inverse!")
+  myinv<- solve(mydata)
   x$setinv(myinv)
-  myinv
+  return(myinv)
 }
-
-
